1c6a253b4d08ff2ca0ae956b7e80bdbb3dd5b7f5
marshall.matt <matthew.marshall@netcompany.com>
2020-12-07 18:20:59 +0000

    Updated Source Credentials

--- CICD/source-credentials.tf
+++ CICD/source-credentials.tf
@@ -4,7 +4,7 @@
 resource "aws_codebuild_source_credential" "github-mpm3278" {
   auth_type   = "PERSONAL_ACCESS_TOKEN"
   server_type = "GITHUB"
-  token       = "977e2918ad414c9976466121b3ddfc66b6eacf22"
+  token       = "f635950f3d9a64198c3967cf9864e525ea2785dc"
 }
 
 
